5818ccf62a07d0c13ea762ffd5fe199d0df6a269
cache-cache-la <rote.blumen.21991@gmail.com>
2020-06-27 20:23:26 +0900

    リスト作成機能追加

--- kanban/urls.py
+++ kanban/urls.py
@@ -7,7 +7,8 @@ app_name = "kanban"
 urlpatterns = [
     path("", views.index, name="index"),
     path("home/", views.home, name="home"),
-    path('signup/', views.signup, name='signup'),
+    path("signup/", views.signup, name="signup"),
     path("users/<int:pk>/", views.UserDetailView.as_view(), name="users_detail"),
     path("users/<int:pk>/update/", views.UserUpdateView.as_view(), name="users_update"),
+    path("lists/create/", views.ListCreateView.as_view(), name="lists_create"),
 ]
--- kanban/views.py
+++ kanban/views.py
@@ -5,9 +5,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, resolve_url
-from django.views.generic import DetailView, UpdateView
-from .forms import UserForm
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, UpdateView, CreateView
+from .forms import UserForm, ListForm
 from .mixins import OnlyYouMixin
+from . models import List
 
 # トップページ
 def index(request):
@@ -46,3 +48,14 @@ class UserUpdateView(OnlyYouMixin, UpdateView):
 
     def get_success_url(self):
         return resolve_url('kanban:users_detail', pk=self.kwargs['pk'])
+
+# リスト作成
+class ListCreateView(LoginRequiredMixin, CreateView):
+    model = List
+    template_name = "kanban/lists/create.html"
+    form_class = ListForm
+    success_url = reverse_lazy("kanban:home")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
