e573508956ad913fda262f423d2ecdf9ca55e1a1
cache-cache-la <rote.blumen.21991@gmail.com>
2020-06-28 22:31:32 +0900

    リスト編集機能追加

--- kanban/views.py
+++ kanban/views.py
@@ -69,3 +69,13 @@ class ListListView(LoginRequiredMixin, ListView):
 class ListDetailView(LoginRequiredMixin, DetailView):
     model = List
     template_name = "kanban/lists/detail.html"
+
+# リスト編集
+class ListUpdateView(LoginRequiredMixin, UpdateView):
+    model = List
+    template_name = "kanban/lists/update.html"
+    form_class = ListForm
+
+    def get_success_url(self):
+        return resolve_url('kanban:lists_detail', pk=self.kwargs['pk'])
+
