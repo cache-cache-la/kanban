9a1ecd19ce7699894df0d0485fa997fbceb94fe2
cache-cache-la <rote.blumen.21991@gmail.com>
2020-06-29 15:24:00 +0900

    superの指定修正

--- kanban/forms.py
+++ kanban/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import List, Card
 
+
 class UserForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
@@ -16,7 +17,7 @@ class UserForm(forms.ModelForm):
 class ListForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
-        super(UserForm, self).__init__(*args, **kwargs)
+        super(ListForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
 
@@ -27,7 +28,7 @@ class ListForm(forms.ModelForm):
 class CardForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
-        super(UserForm, self).__init__(*args, **kwargs)
+        super(CardForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
 
@@ -45,3 +46,14 @@ class CardCreateFromHomeForm(forms.ModelForm):
     class Meta:
         model = Card
         fields = ("title", "description",)
+
+class CardUpdateForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(CardUpdateForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+    class Meta:
+        model = Card
+        fields = ("title", "description", "list")
