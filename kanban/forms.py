7bea8d1edbd432ca14904c00693111d3ca4626dd
cache-cache-la <rote.blumen.21991@gmail.com>
2020-06-29 15:20:35 +0900

    見た目の修正
    この後各機能の動作確認を行い、微調整します。

--- kanban/forms.py
+++ kanban/forms.py
@@ -4,11 +4,21 @@ from .models import List, Card
 
 class UserForm(forms.ModelForm):
 
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = User
         fields = ("username", "last_name", "first_name", "email",)
 
 class ListForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
 
     class Meta:
         model = List
@@ -16,11 +26,21 @@ class ListForm(forms.ModelForm):
 
 class CardForm(forms.ModelForm):
 
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Card
         fields = ("title", "description", "list")
 
 class CardCreateFromHomeForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(CardCreateFromHomeForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
 
     class Meta:
         model = Card
